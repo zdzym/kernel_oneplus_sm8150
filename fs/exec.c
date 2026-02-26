f49316437f08161d8f72816605a9862f85a4b82e
zdzym <41980497+zdzym@users.noreply.github.com>
2026-02-27 00:04:45 +0800

    Add ReSukiSU hooks

--- fs/exec.c
+++ fs/exec.c
@@ -1865,6 +1865,13 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
+#endif
+
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
